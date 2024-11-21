dcf01dd97c52fe6311671626d9abe5197612df9a
Andrey <aa71555443@gmail.com>
2024-11-21 16:57:38 +0500

    added redirects (mirzaev_batyr_bahramovich)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/', // Исходный маршрут
+                destination: '/mirzaev_batyr_bahramovich', // Целевой маршрут
+                permanent: true, // Устанавливает редирект как постоянный (для SEO)
+            },
+        ];
+    },
+};
 
 export default nextConfig;
